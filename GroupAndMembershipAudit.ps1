7ce8da397f8e67f60db26719686caaf7aec901cf
Aubrey Nicklaus <ranicklaus@gmail.com>
2019-09-12 12:51:44 -0400

    Add files via upload

---
+++ GroupAndMembershipAudit.ps1
@@ -0,0 +1,40 @@
+#import activedirectory module
+import-module activedirectory
+
+#Set Execution Policy so that you can run the script
+Set-ExecutionPolicy -scope CurrentUser -ExecutionPolicy RemoteSigned
+
+write-host "Enter partial name of group(s) to list membership" -foregroundcolor yellow
+
+$groupname = read-host "Group Name"
+
+$groupname = $groupname + '*'
+
+$Groups = (Get-AdGroup -filter * | Where { $_.name -like $groupname } | select name -expandproperty name)
+
+$Table = @()
+
+$Record = [ordered]@{
+    "Group"          = ""
+    "SamAccountName" = ""
+    "Name"           = ""
+}
+
+Foreach ($Group in $Groups) {
+
+    $Arrayofmembers = Get-ADGroupMember -identity $Group | select name, samaccountname
+
+    foreach ($Member in $Arrayofmembers) {
+        $Record."Group" = $Group
+        $Record."SamAccountName" = $Member.SamAccountName
+        $Record."Name" = $Member.Name
+        $objRecord = New-Object PSObject -property $Record
+        $Table += $objrecord
+    }
+
+}
+write-host "Enter CSV name for output" -foregroundcolor cyan
+
+$outfile = read-host "CSV Name"
+
+$Table | export-csv "$outfile.csv" -NoTypeInformation
